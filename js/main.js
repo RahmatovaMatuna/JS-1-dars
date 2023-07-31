6e2f318c81d4569145d243795321de71c7002c13
RahmatovaMaftuna <syakubov565@gmail.com>
2023-07-31 16:00:00 +0500

    JS 1-dars
    24-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*console.log(2+2*5-4)*/
+/*alert("Saytimizga xush kelipsiz!")*/
+/*document.write("Men body oynasida ko'rinaman!")*/
+/*let x = 2*/
+/*document.write(x)*/
+
+/*let x = 2*/
+/*let y = 5*/
+/*x = 10*/
+
+/*let x = 10,y = 5, z = 2;*/
+/*document.write(x+y+z);*/
+
+/* Data type in JS*/
+
+/*let x = 5 */                  /*number*/
+/*let matn = "JavaScript"  */   /*string*/
+
+/*document.write(typeof matn)*/
+
+/*Math in JS*/
+/*let x = 5;*/
+/* x+=1; */  /* x=5+1 */
+/* x-=1; */  /* x=5-1 */
+/* x*=1; */  /* x=5*1 */
+/* x/=1; */  /* x=5/1 */
+/* let qoldiq = x%6; */
+/* let daraja = x**3; */  /* 5*5*5 */
+/* ++x; */
+/* --x; */
+/* x++; */
+/* x--; */
+/*document.write(x+5);*/
+
+/* if (agar) */
+/* else(u holda) */
+/* else if (yoki bo'lmasa) */
+
+/*
+if ("salom" == "salom"){
+    console.log("Ular teng!")
+}
+else{
+    console.log("Ular teng emas!")
+}*/
+
+if (45 == 44){
+    console.log(" Birinchisidagi qiymatlar teng !")
+}
+else if (45 == 46){
+    console.log(" Ikkinchisidagi qiymatlar teng !")
+}
+else {
+    console.log(" Ikkinchisidagi qiymatlar teng emas!")
+}
